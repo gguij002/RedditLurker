c6bc565ace94b11ba8f6e1dd2e6d9125271eb10b
gguij002 <gguij002@fiu.edu>
2014-08-03 03:26:55 -0400

    Wroking somewhat listScrol load more. But it crashes after 75 new subreddits

--- src/com/gery/redditlurker/TopRatedFragment.java
+++ src/com/gery/redditlurker/TopRatedFragment.java
@@ -19,14 +19,27 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.AbsListView.OnScrollListener;
 import android.widget.AdapterView;
 import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.Toast;
 
-public class TopRatedFragment extends Fragment {
-	 
+public class TopRatedFragment extends Fragment implements OnScrollListener
+{
+	//List Items 
+	int currentFirstVisibleItem = 0;
+	int currentVisibleItemCount = 0;
+	int totalItemCount = 0;
+	int currentScrollState = 0;
+	boolean loadingMore = false;
+	Long startIndex = 0L;
+	Long offset = 25L;
+	View footerView;
+	//List Items
+	
 	List<SubReddit> subRedditsList;
     private ProgressDialog pDialog;
     View rootView;
@@ -36,21 +49,62 @@ public class TopRatedFragment extends Fragment {
     {
         subRedditsList = new ArrayList<SubReddit>();
         new LoadInbox(inflater.getContext()).execute();
-    	
-	    rootView = inflater.inflate(R.layout.fragment_top_rated, container, false);
-	    return rootView;
+        rootView = inflater.inflate(R.layout.fragment_top_rated, container, false);
+        setOnItemClickListener(inflater.getContext());
+        return rootView;
+    }
+    
+    @Override
+    public void onScroll(AbsListView absListView, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+        this.currentFirstVisibleItem = firstVisibleItem;
+        this.currentVisibleItemCount = visibleItemCount;
+        this.totalItemCount = totalItemCount;
+    }
+
+    @Override
+    public void onScrollStateChanged(AbsListView absListView, int scrollState) {
+        this.currentScrollState = scrollState;
+        this.isScrollCompleted(absListView.getContext());
+    }
+
+    private void isScrollCompleted(Context context) {
+        if (this.currentVisibleItemCount > 0 && this.currentScrollState == SCROLL_STATE_IDLE && this.totalItemCount == (currentFirstVisibleItem + currentVisibleItemCount)) {
+            /*** In this way I detect if there's been a scroll which has completed ***/
+            /*** do the work for load more date! ***/
+            if (!loadingMore) {
+                loadingMore = true;
+                new LoadInbox(context).execute();
+            }
+        }
+    }
+
+    
+    private void setOnItemClickListener(final Context context)
+    {
+    	final ListView storiesListView = (ListView) rootView.findViewById(R.id.all_subreddit_list);
+    	storiesListView.setOnItemClickListener(new OnItemClickListener() {
+ 	         @Override
+ 	         public void onItemClick(AdapterView<?> a, View v, int position, long id) { 
+ 	        	  SubReddit subReddit = (SubReddit) subRedditsList.get(position);
+ 	        	  Intent nextActivity = new Intent(context, SubRedditItemActivity.class);
+             	  nextActivity.putExtra("subReddit", subReddit.link);
+             	  startActivity(nextActivity); 
+ 		     }  
+ 	    });
+    	storiesListView.setOnScrollListener(this);
     }
     
 	/**
 	 * Background Async Task to Load all INBOX messages by making HTTP Request
 	 * */
-	class LoadInbox extends AsyncTask<String, String, String> 
+	class LoadInbox extends AsyncTask<String, String, List<SubReddit>> 
 	{
 		private Context fragmentContext;
 		
 		public LoadInbox(Context context)
 		{
 			this.fragmentContext = context;
+			loadingMore = true;
 		}
 		
 	     /**
@@ -70,10 +124,12 @@ public class TopRatedFragment extends Fragment {
 	    /**
          * getting SubReddits
          * */
-        protected String doInBackground(String... args) 
+        protected List<SubReddit> doInBackground(String... args) 
         {
-        	final String REDDIT_SUBREDDITS_URL = "http://www.reddit.com/reddits/.rss";
-            List<SubReddit> listOfSubReddits = new ArrayList<SubReddit>();
+        	//"http://reddit.com/r/reddits.rss?limit=[limit]&after=[after]";
+        	final String REDDIT_SUBREDDITS_URL = URLCreate(startIndex.intValue(), offset.intValue());
+
+        	List<SubReddit> listOfSubReddits = new ArrayList<SubReddit>();
              
             //Create List Of Subreddits
             Document subRedditsDocument = new RedditRSSReader(REDDIT_SUBREDDITS_URL).execute();
@@ -83,34 +139,36 @@ public class TopRatedFragment extends Fragment {
             	SubReddit item = new SubReddit((Element)listOfSubredditsRaw.item(i)).execute();	
             	listOfSubReddits.add(item);
    	   		}
-            subRedditsList = listOfSubReddits;
-            return null;
+            subRedditsList.addAll(startIndex.intValue(),listOfSubReddits);
+            return subRedditsList;
         }
- 
+        
+        private String URLCreate(int startIndex, int offset)
+        {
+        	return "http://www.reddit.com/reddits/.rss" +  "?limit=" + offset + "&after="+ startIndex;
+        }
+        
         /**
          * After completing background task Dismiss the progress dialog
          * **/
-        protected void onPostExecute(String file_url) {
+        protected void onPostExecute(final List<SubReddit> listOfSubReddits) {
             // dismiss the dialog after getting all products
             pDialog.dismiss();
-            final ListView storiesListView = (ListView) rootView.findViewById(R.id.all_subreddit_list);
-            storiesListView.setOnItemClickListener(new OnItemClickListener() {
-      	         @Override
-      	         public void onItemClick(AdapterView<?> a, View v, int position, long id) { 
-      	        	  SubReddit subReddit = (SubReddit) subRedditsList.get(position);
-      	        	  Intent nextActivity = new Intent(fragmentContext, SubRedditItemActivity.class);
-                  	  nextActivity.putExtra("subReddit", subReddit.link);
-                  	  startActivity(nextActivity); 
-      		     }  
-      	    });
+            loadingMore = false;
+            if (listOfSubReddits.size() > 0) {
+                startIndex = startIndex + listOfSubReddits.size();
+            }
+           
             
+            final ListView storiesListView = (ListView) rootView.findViewById(R.id.all_subreddit_list);
 	        // updating UI from Background Thread
             getActivity().runOnUiThread(new Runnable() {
                 public void run() {
-                	SubRedditCustomBaseAdapter var = new SubRedditCustomBaseAdapter(fragmentContext, subRedditsList);
+                	SubRedditCustomBaseAdapter var = new SubRedditCustomBaseAdapter(fragmentContext, listOfSubReddits);
                 	storiesListView.setAdapter(var);
             	}
             });
+            super.onPostExecute(listOfSubReddits);
         }
 	}
 }
