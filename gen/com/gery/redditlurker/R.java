4cc286b267004e89421cc3ff46fff7c6a9af03ce
gguij002 <gguij002@fiu.edu>
2014-09-04 20:00:42 -0400

    Added new Images

--- gen/com/gery/redditlurker/R.java
+++ gen/com/gery/redditlurker/R.java
@@ -23,10 +23,12 @@ public final class R {
     public static final class drawable {
         public static final int ic_action_search=0x7f020000;
         public static final int ic_chat_image=0x7f020001;
-        public static final int ic_go_button=0x7f020002;
-        public static final int ic_launcher=0x7f020003;
-        public static final int ic_launcherd=0x7f020004;
-        public static final int ic_search_button=0x7f020005;
+        public static final int ic_comments_image=0x7f020002;
+        public static final int ic_delete_image=0x7f020003;
+        public static final int ic_go_button=0x7f020004;
+        public static final int ic_launcher=0x7f020005;
+        public static final int ic_launcherd=0x7f020006;
+        public static final int ic_search_button=0x7f020007;
     }
     public static final class id {
         public static final int action_fav=0x7f09001b;
